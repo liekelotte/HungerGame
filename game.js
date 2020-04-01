6835b2ca284f63425463558c8408c9cfa1f868cc
liekelotte <liekelotte@gmail.com>
2020-04-01 16:57:27 +0900

    added the necessary libraries and implemented the questions

--- game.js
+++ game.js
@@ -1,3 +1,32 @@
 if (jQuery) {
+  var checkAnswers = function(){ 
+    var answerString = ""; 
+    var answers = $(":checked"); 
+    answers.each(function(i) { 
+      answerString = answerString + answers[i].value; 
+    }); 
+    $(":checked").each(function(i) { 
+      var answerString = answerString + answers[i].value; 
+    }); 
+    checkIfCorrect(answerString); 
+  }; 
+  
+  var checkIfCorrect = function(theString){ 
+    if(parseInt(theString, 16) === 616162){ 
+      $("body").addClass("correct"); 
+      $("h1").text("You Win!"); 
+      $("canvas").show(); 
+    } 
+  };
+
   $("#question1").show();
-}
+};
+
+// if(impress){
+//   $("#question2").show();
+// };
+
+// if(atom){
+//   $("question3").show();
+// };
+
